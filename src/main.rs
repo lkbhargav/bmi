896b3bd169baf6dc8abdce44d7fe9b3155b8ac56
Bhargav Lakkur Kusha Kumar <lkbhargav9@gmail.com>
2021-06-04 16:39:30 -0400

    Removed commented code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-extern crate reqwest;
-
 mod bmi;
 mod extensions;
 
 use bmi::BMI;
 use extensions::{FloatExtensions, StringExtensions};
-use reqwest::blocking::ClientBuilder;
-use reqwest::redirect;
-use reqwest::Url;
-use std::cell::RefCell;
-use std::collections::HashMap;
 use std::io::stdin;
 use std::process::exit;
-use std::sync::{Arc, Mutex};
 
 fn get_user_input(question: &str) -> String {
     println!("{}", question);
@@ -50,21 +42,4 @@ fn main() {
         bmi.get_bmi(),
         bmi.get_range()
     );
-
-    // let resp = reqwest::blocking::get("https://httpbin.org/ip").unwrap()
-    //     .json::<HashMap<String, String>>().unwrap();
-    // println!("{:#?}", resp);
-
-    // let mut status_codes = Vec::<u16>::new();
-    // let mut redirect_urls = Vec::<String>::new();
-
-    // let client = reqwest::blocking::Client::builder()
-    // .redirect(reqwest::redirect::Policy::custom( |attempt| {
-    //     let next_url = attempt.url().to_string();
-    //     let status_code = attempt.status();
-    //     println!("Next URL: {} | Status code: {}", next_url, status_code);
-    //     attempt.follow()
-    // })).build().unwrap();
-
-    // let resp = client.get("https://go.bluegreenanalytics.com").send().unwrap();
 }
